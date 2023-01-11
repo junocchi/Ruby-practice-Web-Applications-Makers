73168bf4e7ed882011cd66d17c04921083208bdd
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-01-11 11:44:02 +0000

    Finished Building routes

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -7,4 +7,22 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  get '/' do
+    name = params[:name]
+  return "Hello #{name}!"
+  end
+
+  post '/hello' do
+    name = params[:name]
+  return "Hello #{name}!"
+  # The text content above (string) is sent as a request body
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+  return "Thanks #{name}, you sent this message: #{message}!"
+  # The text content above (string) is sent as a request body
+  end
 end
